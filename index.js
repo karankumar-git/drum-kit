842f35dedaaa8657d14686501b04c9c58e035f05
Karan Kumar <74554025+karankumar-git@users.noreply.github.com>
2021-12-24 18:51:30 +0530

    add animation while pressing drum button

--- index.js
+++ index.js
@@ -20,9 +20,21 @@ for (var i = 0; i < buttonList.length; i++){
     buttonList[i].addEventListener("click", function () {
         playaudio(this.innerHTML);
         console.log(this.innerHTML);
+        pressAnimation(this.innerHTML);
     });
 };
 
-document.addEventListener("keypress", function (event) {
-    playaudio(event.key);
-})
+document.addEventListener("keypress", function (event) {    //it gives the info about keypress
+    playaudio(event.key);   //it passes the value correspond to key.
+    pressAnimation(event.key);
+});
+
+function pressAnimation(drumname) {
+    var drumSelector = "." + drumname;
+    const drum = document.querySelector(drumSelector);
+    console.log(drum);
+    drum.classList.add("pressed");    
+    setTimeout(() => {
+        drum.classList.remove("pressed");
+    }, 100);
+}
